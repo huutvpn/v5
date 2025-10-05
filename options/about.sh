fa40135e643aeb217d85403953cccfab26fbc360
huutvpn <143250398+huutvpn@users.noreply.github.com>
2025-10-05 17:36:16 +0700

    Add files via upload

---
+++ options/about.sh
@@ -0,0 +1,23 @@
+ echo -e "    
+                                                                         
+ " | lolcat
+
+
+echo -e "┌─────────────────────────────────────────────────┐" | lolcat
+echo -e "│               AUTOSCRIPT PREMIUM                │" | lolcat
+echo -e "│             BY ANDYYUDA              │" | lolcat
+echo -e "└─────────────────────────────────────────────────┘" | lolcat
+
+echo -e "> Support OS 
+  For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+  For Debian 9 & Debian 10 & Debian 11 " | lolcat
+echo ""
+echo -e "  Contact Owner AutoScript
+> WA: 082131861788
+> TTD ANDYYUDA" | lolcat
+echo ""
+echo -e "Thanks for your Order ANDYYUDA" | lolcat
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
